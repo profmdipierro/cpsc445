928067304b3815c7d5d8eabc241f563f5b539e26
Massimo Di Pierro <prof.massimo.dipierro@gmail.com>
2021-09-11 21:27:57 +0000

    buggy alg2

--- class02/alg2.py
+++ class02/alg2.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import socket
@@ -6,40 +7,16 @@ import random
 import signal
 
 
-class Pipe(threading.Thread):
+class Pipe:
     def __init__(self, conn):
-        threading.Thread.__init__(self)
         self.conn = conn
-        self.queue = None
-        
-    def run(self):
-        while True:            
-            message = self.conn.recv(1000)
-            if message:
-                self.queue = message
-            time.sleep(0.0001)
+
+    def send(self, message):
+        self.conn.send(message.encode())
 
     def recv(self):
-        print(sys.argv[1], self.queue)
-        while not self.queue:
-            time.sleep(0.0001)
-        print(sys.argv[1], "recv ...")
-        message, self.queue = self.queue, None
-        return message
+        return self.conn.recv(100).decode()
 
-def create_server(rank, p, port):
-    pipes = {}
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("binding to", port)
-    sock.bind(("127.0.0.1", port))
-    sock.listen(100)
-    for k in range(p):
-        if k != rank:
-            conn, address = sock.accept()
-            pipe = Pipe(conn)
-            pipe.start()
-            pipes[k] = pipe
-    return pipes
 
 class Server(threading.Thread):
     def __init__(self, rank, p, port):
@@ -51,7 +28,16 @@ class Server(threading.Thread):
         self.receivers = {}
 
     def run(self):
-        self.receivers = create_server(self.rank, self.p, self.port + self.rank)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("binding to", self.port + self.rank)
+        self.sock.bind(("127.0.0.1", self.port + self.rank))
+        self.sock.listen(self.p - 1)
+        for k in range(self.p):
+            if k != self.rank:
+                conn, address = self.sock.accept()
+                pipe = Pipe(conn)
+                self.receivers[k] = pipe
+        print("server stopped")
 
     def connect_clients(self):
         time.sleep(3)
@@ -59,38 +45,42 @@ class Server(threading.Thread):
             if k != self.rank:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.connect(("127.0.0.1", self.port + k))
-                self.senders[k] = sock
+                self.senders[k] = Pipe(sock)
                 print(self.rank, "connected to", k)
-        time.sleep(3)
-        
+
+
 def main():
     rank = int(sys.argv[1])
     p = int(sys.argv[2])
     print(f"{rank}/{p}")
 
-    server = Server(rank, p, 9000)
+    server = Server(rank, p, 9200)
     server.start()
     server.connect_clients()
+    server.join()
 
     print(rank, "ready!")
-    message = b"hello"
+    message = "hello"
 
     if rank == 0:
-        for k in range(1,p):
-            print(rank,"sending to", k)
-            server.senders[k].send(message)
-            print(rank,"receiving from", k)
-            message = server.receivers[k].recv()
-            print(rank, "received from", k, message)
+        print(rank, "sending to", 1)
+        server.senders[1].send(message)
+        print(rank, "receiving from", p - 1)
+        message = server.receivers[p - 1].recv()
+        print(rank, "received from", p - 1, message)
     else:
-        print(rank,"receiving from", 0)
-        message = server.receivers[0].recv()
-        print(rank, "received", message)
-        print(rank,"sending to", 0)
-        server.senders[0].send(message)
+        previous = rank - 1
+        print(rank, "receiving from", previous)
+        message = server.receivers[previous].recv()
+        print(rank, "received from", previous, message)
+        message = message + "."
+        next = (rank + 1) % p
+        print(rank, "sending to", next, message)
+        server.senders[next].send(message)
 
     time.sleep(10)
     print(rank, "done")
-                       
+
+
 if __name__ == "__main__":
     main()
