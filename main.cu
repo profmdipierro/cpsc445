266229050eab049b6e80eba6da3e7c14e185e075
Massimo Di Pierro <prof.massimo.dipierro@gmail.com>
2021-11-18 04:25:14 +0000

    test

--- main.cu
+++ main.cu
@@ -23,17 +23,18 @@ __global__ void reduce_sum_step1(int * da, int N) {
   }  
 }
 
-/*
-__global__ void reduce_sum(int * da, int N) {
-  int B = gridDim.x;
-  int W = blockDim.x;
-  int shift = W * B;
-
-  int gid = blockIdx.x* blockDim.x + threadIdx.x;
-  
-  for(int i=gid+shift; i<N; i+=shift) da[gid]+=da[i];
+__global__ void reduce_sum_step2(int * da, int W) {
+  int B = blockDim.x;
+  int shift = B;
+  int tid = threadIdx.x;
+  for(int delta=1; delta<B; delta*=2) {    
+    int i = tid*2*delta;
+    if (i + delta < B) {
+      da[i*W] += da[(i+delta)*W];
+    }
+    __syncthreads();
+  }  
 }
-*/
 
 int main() {
   //INPUTS
@@ -55,17 +56,19 @@ int main() {
   int W = 16;
   reduce_sum_step1<<<B,W>>>(da, N);
   cudaDeviceSynchronize();
+  reduce_sum_step2<<<1,B>>>(da, W);
+  cudaDeviceSynchronize();
 
   int sum;
   // cudaMemcpy(&sum, da, sizeof(int), cudaMemcpyDeviceToHost);
   cudaMemcpy(hb, da, N*sizeof(int), cudaMemcpyDeviceToHost);
 
   printf("%i\n", hb[0]);
-  printf("%i\n", hb[16]);
-  printf("%i\n", hb[32]);
+  // printf("%i\n", hb[16]);
+  // printf("%i\n", hb[32]);
   // printf("%i\n", hb[32]);
 
-  sum = hb[0] + hb[16] + hb[32]; //  + hb[32];
+  sum = hb[0]; //  + hb[32];
   int expected_sum =  (N-1)*N*(2*N-1)/6;
   printf("%i (should be %i)", sum, expected_sum);
   cudaFree(da);
