12ee56c08c1f1308e197137678063051d8dab144
Massimo Di Pierro <prof.massimo.dipierro@gmail.com>
2021-11-18 03:52:47 +0000

    test

--- main.cu
+++ main.cu
@@ -1,30 +1,43 @@
 #include <stdio.h>
 
+__global__ void reduce_sum_part1(int * da, int N) {
+  int B = gridDim.x;
+  int W = blockDim.x;
+  int shift = W * B;
+
+  int gid = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  for(int i=gid+shift; i<N; i+=shift) da[gid]+=da[i];
+}
+
+/*
 __global__ void reduce_sum(int * da, int N) {
   int B = gridDim.x;
   int W = blockDim.x;
-  int shift = B*W;
+  int shift = W * B;
 
-  int gid = blockIdx.x * blockDim.x + threadIdx.x;
+  int gid = blockIdx.x* blockDim.x + threadIdx.x;
   
   for(int i=gid+shift; i<N; i+=shift) da[gid]+=da[i];
   __syncthreads();
 
 
   for(int delta=1; delta<shift; delta*=2) {
-    int i = gid*2*delta;
+    int i = tid*2*delta;
     if (i + delta < N) {
       da[i] += da[i+delta];
     }
     __syncthreads();
   }
 }
+*/
 
 int main() {
   //INPUTS
   int N = 1000;
 
   int *ha = new int[N];
+  int *hb = new int[N];
   int *da;
   cudaMalloc((void **)&da, N*sizeof(int));
 
@@ -41,11 +54,18 @@ int main() {
   cudaDeviceSynchronize();
 
   int sum;
-  cudaMemcpy(&sum, da, sizeof(int), cudaMemcpyDeviceToHost);
+  // cudaMemcpy(&sum, da, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpy(hb, da, N*sizeof(int), cudaMemcpyDeviceToHost);
 
+  printf("%i\n", hb[0]);
+  printf("%i\n", hb[16]);
+  printf("%i\n", hb[32]);
+
+  sum = hb[0] + hb[16] + hb[32];
   int expected_sum = (N-1)*N*(2*N-1)/6;
   printf("%i (should be %i)", sum, expected_sum);
   cudaFree(da);
   free(ha);
+  free(hb);
   return 0;
 }
